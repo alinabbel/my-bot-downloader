4539031447a43707961d2ecc0e72d4dee20c7e22
alinabbel <sada7267@gmail.com>
2026-03-27 05:53:41 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,16 @@
 from flask import Flask
 from threading import Thread
 
-app = Flask('')
+app = Flask(’’)
 
-@app.route('/')
+@app.route(’/’)
 def home():
-    return "I am alive!"
+return “البوت شغال! 🤖”
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+app.run(host=‘0.0.0.0’, port=8080)
 
 def keep_alive():
-    t = Thread(target=run)
-    t.start()
+t = Thread(target=run)
+t.daemon = True
+t.start()
